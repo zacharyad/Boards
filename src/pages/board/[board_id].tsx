e0708ed7df641d36674798d26e2378a2bbfb5f81
zacharyad <zacharyadcoding@gmail.com>
2022-10-13 19:27:27 -0400

    trying to fix conditional render occuring in localhost and not on deploy

--- src/pages/board/[board_id].tsx
+++ src/pages/board/[board_id].tsx
@@ -6,12 +6,13 @@ import StyleCard from "../../components/StyleCard";
 const Board: NextPage = () => {
     const router = useRouter()
     const { board_id } = router.query
+
     
     return (
         <>
         <div className="container mx-auto flex max-h-screen min-h-screen min-w-full flex-col items-center justify-center bg-indigo-100">
             <StyleCard name="Board PAGE"  
-                    desc={board_id || ""}
+                    desc={board_id ? board_id : ""}
                     linkPath="/"
                     linkText="Home"/>
         </div>
